3b05e2e88439613bcbfb69c0f01e8d04e04578b9
anirudhsai2 <anirudhsai2@gmail.com>
2026-02-18 00:19:30 +0530

    chore: string questions

---
+++ strings/WordsContainingChar02.java
@@ -0,0 +1,31 @@
+package strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Find Words Containing Character
+ * =================================
+ * You are given a 0-indexed array of strings words and a character x.
+ *
+ * Return an array of indices representing the words
+ * that contain the character x.
+ *
+ * Note that the returned array may be in any order.
+ *
+ * Time Complexity: O(n * m)
+ * Space Complexity: O(n)
+ */
+public class WordsContainingChar02 {
+    List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+}
